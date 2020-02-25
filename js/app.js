f77ce270d90a8a4df13bf6c0f088f19330726db7
a <a@a.pl>
2020-02-25 22:47:43 +0100

    Progress - new views and done home view for mobile

--- js/app.js
+++ js/app.js
@@ -1,55 +1,14 @@
-window.addEventListener("load", () => {
-  const el = $("#app");
+'use strict';
 
-  // Compile Handlebar Templates
-  const firstTemplate = Handlebars.compile($("#first-template").html());
-  const secondTemplate = Handlebars.compile($("#second-template").html());
-  const thirdTemplate = Handlebars.compile($("#third-template").html());
-  const fourthTemplate = Handlebars.compile($("#fourth-template").html());
-  const homeTemplate = Handlebars.compile($("#home-template").html());
-
-  const html = homeTemplate();
-  el.html(html);
-  // Router Declaration
-  const router = new Router({
-    mode: "history",
-    page404: (path) => {
-      const html = homeTemplate();
-      el.html(html);
-    },
-  });
-
-  router.add("/", () => {
-    let html = homeTemplate();
-    el.html(html);
-  });
-
-  router.add("/first", () => {
-    let html = firstTemplate();
-    el.html(html);
-  });
-
-  router.add("/second", () => {
-    let html = secondTemplate();
-    el.html(html);
-  });
-
-  router.add("/third", () => {
-    let html = thirdTemplate();
-    el.html(html);
-  });
-
-  // Navigate app to current url
-  router.navigateTo(window.location.pathname);
-
-  
-  $("a").on("click", event => {
-    // Block browser page load
-    event.preventDefault();
-
-    // Navigate to clicked url
-    const href = target.attr("href");
-    const path = href.substr(href.lastIndexOf("/"));
-    router.navigateTo(path);
-  });
-});
+(function() {
+    function init() {
+        var router = new Router([
+            new Route('home', 'home.html', true),
+            new Route('first', 'first.html'),
+            new Route('second', 'second.html'),
+            new Route('third', 'third.html'),
+            new Route('fourth', 'fourth.html')
+        ]);
+    }
+    init();
+}());
